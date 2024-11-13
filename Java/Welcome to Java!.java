ab4ab7f1ac3e4054f5e3767cfcf5ec8fd4de590e
Abhishek Prashant Chandurkar <cabhishek1304@gmail.com>
2024-11-13 22:54:39 +0530

    Welcome to Java!

---
+++ Java/Welcome to Java!.java
@@ -0,0 +1,11 @@
+// Change the name of the file to "Solution.java"
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Print output to STDOUT. Your class should be named Solution. */
+        // Each call to Sytem.out.println() utomatically ends with a newline
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
+
